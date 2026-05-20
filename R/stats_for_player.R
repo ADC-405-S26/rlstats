ac96439b89e354f17c3738f67a92bc0e705320a3
Tristan Zook <tristanzook@gmail.com>
2026-05-20 04:13:54 +0000

    Initial commit. Contains base package files as well as one function with documentation and assertions

---
+++ R/stats_for_player.R
@@ -0,0 +1,35 @@
+#' Calculates some basic aggregate stats for a specified player
+#'
+#' @param df A data frame with data from the rocket league stats api
+#' @param name A string specifying the gamer-tag of the desired player
+#' @param game Optional. A string specifying the guid of the desired match to analyze. Otherwise aggregates over all matches.
+#'
+#' @returns Some aggregate statistics for the specified player
+#' @importFrom dplyr filter summarize n
+#' @importFrom rlang .data
+#' @export
+#'
+#' @examples
+#' stats_for_player(rocket_data, name = "TDMZ223")
+#'
+#' stats_for_player(rocket_data, name = "TDMZ223", "C34377FE11F153C39C443797FB07660C")
+stats_for_player <- function(df,name,game = NULL)
+{
+  checkmate::assert_data_frame(df)
+  checkmate::assert_string(name)
+  checkmate::assert_string(game, null.ok = TRUE)
+
+  p <- if(is.null(game)){
+    df |>  filter(.data$player_name == name)
+  } else{
+    df |>  filter(.data$player_name == name, .data$match_guid == game)
+  }
+
+  p |>   summarize(Number_of_Ball_Touches = sum(.data$touches),
+      Number_Of_Goals = sum(.data$goals),
+      Number_Of_Saves = sum(.data$saves),
+      Average_Speed = mean(.data$speed, na.rm = TRUE),
+      Average_Boost = mean(.data$boost, na.rm = TRUE),
+      Percentage_Of_Time_Supersonic = sum(.data$is_supersonic == 'True')/n())
+  }
+
